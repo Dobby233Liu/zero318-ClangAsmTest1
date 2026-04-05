ef4ba73ba966d28524f5ca02c6f9e6f838dcec0b
Liu Wenyuan <15816141883@163.com>
2026-04-05 23:13:10 +0800

    Less invasive changes

--- dobby/configure.cmd
+++ dobby/configure.cmd
@@ -11,11 +11,13 @@ cmake ^
     -G "MinGW Makefiles" -DCMAKE_MAKE_PROGRAM="D:\sdks\w64devkit-x86\bin\make.exe" ^
     --toolchain dobby\x86toolchain.cmake ^
     -DCLANG_TYPE="CLANG-CL" -DCUSTOM_CLANG="false" -DIS_X64="false" ^
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
     -DCMAKE_SYSTEM_VERSION="10.0.20348.0" ^
+    -DCMAKE_IS_BROKEN=false ^
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
     -DCMAKE_C_FLAGS_RELWITHDEBINFO="%ccflags%" ^
     -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="%ccflags%" ^
     -DSIZE_OPTIMIZE="false" -DSTATIC_CSOUND="false" ^
+    %* ^
     ..
 set exitlevel=%ERRORLEVEL%
 popd
