ed5e56a8d1bcd68dfcd2906023a4ad320126d9a8
Liu Wenyuan <15816141883@163.com>
2026-04-05 23:52:48 +0800

    Presets
    cmake --preset x86-Clang-Release-Debug-dobby
    cmake --build --preset UMTest1-dobby

--- dobby/make_um.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-pushd %~dp0..
-cmake --build out\build -t UMTest1
-popd
--- dobby/toolchain_x86_personal.cmake
+++
@@ -1,3 +0,0 @@
-set(VS_INSTALLATION_PATH "D:/tools/Program Files/Microsoft Visual Studio/2022/BuildTools")
-set(CMAKE_ASM-ATT_COMPILER "D:/sdks/w64devkit-x86/bin/as.exe")
-include("${CMAKE_CURRENT_LIST_DIR}/toolchain_x86.cmake")
